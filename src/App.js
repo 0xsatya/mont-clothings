40ae32d1b8dd7d5da0ec00426c2c0afe644a06af
satya-codes <63343224+satya-codes@users.noreply.github.com>
2020-04-16 14:51:10 +0530

    added components and added styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import HomePage  from "./components/pages/hompage/homepage.component";
+
 function App() {
   return (
-    <div>Hello..</div>
+    <div>
+      <HomePage></HomePage>
+    </div>
   );
 }
 
 export default App;
+ 
