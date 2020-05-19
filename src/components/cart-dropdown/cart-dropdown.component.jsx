8385a8700aed5165e46e42b49c5785bc4e71d5d2
satya-codes <63343224+satya-codes@users.noreply.github.com>
2020-05-19 18:53:45 +0530

    added lib reselect and replaced cart selectros with memoization

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import CustomButton from "../custom-button/custom-button.component";
 import CartItem from "../cart-item/cart-item.component";
+import { selectCartItems } from "../../redux/cart/cart.selectors";
+
 import "./cart-dropdown.styles.scss";
+
 
 import { connect } from "react-redux";
 
@@ -16,7 +19,7 @@ const CartDropdown = ({ cartItems }) => (
   </div>
 );
 
-const mapStateToProps = ({ cart }) => ({
-  cartItems: cart.cartItems,
+const mapStateToProps = (state) => ({
+  cartItems: selectCartItems(state)
 });
 export default connect(mapStateToProps)(CartDropdown);
